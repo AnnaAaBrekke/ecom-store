ac9d4acdff3d86a6006818a444260b5aeb9e4012
AnnaAaBrekke <annabrekke98@gmail.com>
2025-02-10 13:36:48 +0100

    Start of fetching products

---
+++ src/api/products.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+const url = "https://v2.api.noroff.dev/online-shop"; //Env?
+
+const Products = () => {};
+
+export default Products;
+
+// https://content.noroff.dev/front-end-frameworks/api-calls.html
