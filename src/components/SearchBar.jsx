bb36a7a33f2df76bfcdf9f7f2954e5f2ca880d9d
AnnaAaBrekke <annabrekke98@gmail.com>
2025-02-12 15:46:05 +0100

    Sources added if AutoComplete is to be used as well

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 
-// Source: https://salehmubashar.com/blog/create-a-search-bar-in-react-js
+// Source: https://salehmubashar.com/blog/create-a-search-bar-in-react-js (TextField)
+
 
 const SearchBar = ({ searchInput, setSearchInput }) => {
   const handleChange = (e) => {
@@ -25,3 +26,69 @@ const SearchBar = ({ searchInput, setSearchInput }) => {
 };
 
 export default SearchBar;
+
+
+// Source: If I also want to use AutoComplete: https://www.geeksforgeeks.org/how-to-add-auto-complete-search-box-in-reactjs/
+
+// AutocompleteSearch.js
+// import React, { useState } from 'react';
+// import Autosuggest from 'react-autosuggest';
+// import './style.css'; // Import CSS file for custom styling
+
+// const AutocompleteSearch = () => {
+//   const [value, setValue] = useState('');
+//   const [suggestions, setSuggestions] = useState([]);
+
+//   // Mock data for suggestions
+
+
+//   const getSuggestions = (inputValue) => {
+//     const regex = new RegExp(inputValue.trim(), 'i');
+//     return languages.filter((language) => regex.test(language));
+//   };
+
+//   const onSuggestionsFetchRequested = ({ value }) => {
+//     setSuggestions(getSuggestions(value));
+//   };
+
+//   const onSuggestionsClearRequested = () => {
+//     setSuggestions([]);
+//   };
+
+//   const onChange = (event, { newValue }) => {
+//     setValue(newValue);
+//   };
+
+//   const getSuggestionValue = (suggestion) => suggestion;
+
+//   const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
+
+//   const inputProps = {
+//     placeholder: 'Type to search...',
+//     value,
+//     onChange
+//   };
+
+//   // Custom theme for styling
+//   const theme = {
+//     container: 'autocomplete-container',
+//     suggestionsContainer: 'suggestions-container',
+//     suggestion: 'suggestion',
+//     suggestionHighlighted: 'suggestion-highlighted',
+//     input: 'autocomplete-input'
+//   };
+
+//   return (
+//     <Autosuggest
+//       suggestions={suggestions}
+//       onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+//       onSuggestionsClearRequested={onSuggestionsClearRequested}
+//       getSuggestionValue={getSuggestionValue}
+//       renderSuggestion={renderSuggestion}
+//       inputProps={inputProps}
+//       theme={theme} // Apply custom theme for styling
+//     />
+//   );
+// };
+
+// export default AutocompleteSearch;
