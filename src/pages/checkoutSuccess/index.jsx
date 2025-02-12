85b6f518df0a42cdbdf78b0ad7c406b86f566e1d
AnnaAaBrekke <annabrekke98@gmail.com>
2025-02-12 12:39:24 +0100

    Clear cart, displays success message and link to go back

--- src/pages/checkoutSuccess/index.jsx
+++ src/pages/checkoutSuccess/index.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-
-// Add the useCart state total
+import React, { useEffect } from "react";
+import useCart from "../../stores/cartStore";
+import { Link } from "react-router-dom";
 
 const CheckoutSuccessPage = () => {
+  const clearCart = useCart((state) => state.clearCart);
+
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
+
+  
   return (
     <div>
-      <h1>Checkout Success</h1>
+      <h1>Checkout Success!</h1>
+      <h2>Your order has been placed successfully.</h2>
+      <p>Your cart is now empty.</p>
+      <Link to="/">Go Back To Shopping</Link>
     </div>
   );
 };
